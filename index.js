8cbef533c55032723ddecb82be04af2b3a025422
IsiahDB <IsaiahBanks500@gmail.com>
2022-03-18 21:47:05 -0500

    new changes to node

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
-console.log("Hello Node")
-console.log("Goodbye Node Node")
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+app.use(morgan('common'));
+
+
+
+let topMovies = [
+    {
+      title: 'Black Hawk Down'
+    },
+    {
+      title: 'Ace Ventura: When Nature Calls'
+    },
+    {
+      title: 'Wall-E'
+    },
+    {
+      title: "Star Trek"
+    },
+    {
+      title: "Star Wars"
+    },
+    {
+      title: "Dragon Ball Z Broly: The Legendary Super Sayian"
+    },
+    {
+      title: "Water Boy"
+    },
+    {
+      title: "Matrix"
+    },
+    {
+      title: "Ong Bak 2 - The Elephant Lord"
+    },
+    {
+      title: "Bad Boys"
+    }
+  ];
+  
+  // GET requests
+  app.use(express.static('public'));
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to the movie theater!');
+  });
+
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+
+  app.use((err, req, res, next) => {
+    console.err(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
